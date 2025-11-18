9028ff6c5b3203d7c488406c1cb16fd116dc737d
Sebastian Gordon <sebgordon99@gmail.com>
2025-11-18 14:58:10 +1100

    mini project 2

---
+++ Mini Project 2/src/APItest.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export function ApiTest() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.scryfall.com/cards/search?order=cmc&q=c%3Ared+pow%3D10")
+      .then((res) => res.json())
+      .then((json) => setData(json.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Items</h1>
+      <div>
+        {data.map((card) => (
+            <>
+            <div key={card.id}>{card.name}</div>
+            <div>
+                <img src={card.image_uris.small} />
+                </div>
+            </>
+        ))}
+        
+      </div>
+    </div>
+  );
+}
---
+++ Mini Project 2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { ApiTest } from "./APItest";
+
+function App() {
+
+  return (
+    <>
+      <div>text</div>
+      <ApiTest />
+      <p className="read-the-docs">
+        Created by Sebastian Gordon as his second Mini-Project in 2025
+      </p>
+    </>
+  );
+}
+
+export default App;
